cf6f1a4ff1c9e03b5239f58c99dac8c67f31932b
dpajaro98 <drpajaro7@misena.edu.co>
2022-08-18 09:20:21 -0500

    ejercicio de objetos

---
+++ ejercicio-objetos.js
@@ -0,0 +1,22 @@
+//creando el objeto
+let persona={
+    nombre:"Darien",
+    apellido:"Pajaro",
+    año_de_nacimiento:new Date(1998,09,08).getFullYear(),
+    fecha_actual:new Date().getFullYear(),
+    
+    //creando el metodo de nombre
+    nombre_completo:function() {
+        //se usa la palabra this para acceder a los atributos
+        //del objeto
+        return this.nombre+" "+this.apellido
+        
+    },
+    //aqui cree el metodo para calcular la edad
+    edad:function(){
+        return this.fecha_actual-this.año_de_nacimiento
+    }
+}
+console.log(persona.nombre_completo())
+console.log(persona.edad())
+console.log(persona.nombre_completo()+" tiene la edad de "+persona.edad()+" años")
