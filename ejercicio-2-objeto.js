5f3536b629b084adb0dcbee09f0865080ba1139f
dpajaro98 <drpajaro7@misena.edu.co>
2022-08-18 11:50:04 -0500

    en este codigo se añaden los conceptos de get y set en el nombre

--- ejercicio-2-objeto.js
+++ ejercicio-2-objeto.js
@@ -4,6 +4,7 @@ let persona={
     apellido:"Pajaro",
     año_de_nacimiento:new Date(1998,09,08).getFullYear(),
     fecha_actual:new Date().getFullYear(),
+    empresa:"rf",
     
     //creando el metodo de nombre
     nombre_completo:function() {
@@ -15,5 +16,26 @@ let persona={
     //aqui cree el metodo para calcular la edad
     edad:function(){
         return this.fecha_actual-this.año_de_nacimiento
+    },
+    get metodo_n_c(){
+        return this.nombre+" "+this.apellido
+    },
+    get e(){
+        return this.fecha_actual-this.año_de_nacimiento
+
+    },
+    get emp(){
+        return this.empresa.toUpperCase()
+    }
+    ,
+    set emp(emp){
+        return this.empresa=emp.toUpperCase()
+
     }
 }
+
+console.log(persona.metodo_n_c)
+console.log(persona.e)
+
+persona.emp="gb"
+console.log(persona.emp)
