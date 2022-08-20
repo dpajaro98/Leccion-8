fceb586482e9db360d69bd115ddadf0d67f47ba4
dpajaro98 <drpajaro7@misena.edu.co>
2022-08-20 11:45:26 -0500

    cambios

--- ejercicio-clases.js
+++ ejercicio-clases.js
@@ -1,25 +1,48 @@
 class persona {
   //la clase necesita un constructor
   constructor(nombre, apellido, cel,año) {
-    this.nombre = nombre;
-    this.apellido = apellido;
+    this._nombre = nombre;
+    this._apellido = apellido;
     this.cel = cel;
-    this.año=año;
-    this.hoy= new Date().getFullYear()
-   
-    
+    this._año_nac=año;
+    this.hoy= new Date().getFullYear()   
   }
   
   //en este caso se realizo el metodo de nombre con un get
-  
-  get n_c() {
-    return this.nombre + " " + this.apellido;
+    get n_c() {
+    return this._nombre + " " + this._apellido;
   }
-
-  //ahora se va a calcular la edad con otro metodo get
+  //ahora se va a calcular la edad con el metodo get
+  //getter edad
   get edad(){    
-    return this.hoy -this.año
+    return this.hoy -this._año_nac
  }
+ //setter año de nacimiento
+ set año(año){
+    return this._año_nac=año
+ }
+ 
+ //getter nombre
+ get nombre(){
+    return this._nombre
+
+ }
+ //setter nombre
+ set nombre(nombre){
+    return this._nombre=nombre
+
+ }
+  //getter apellido
+  get apellido(){
+    return this._nombre
+
+ }
+ //setter apellido
+ set apellido(apellido){
+    return this._apellido=apellido
+
+ }
+ 
 }
 
 let darien = new persona("darien", "pajaro", 3122345,new Date(1998,09,08).getFullYear());
@@ -28,3 +51,6 @@ console.log(darien);
 console.log(darien.n_c);
 
 console.log(darien.edad)
+
+darien._nombre="amigos dar"
+console.log(darien)
